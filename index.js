5d4d1689d90da69ad5df58d093b9eaf28f99de64
IDAGIZA <ruthidagiza@gmail.com>
2022-06-14 01:22:04 +0300

    commit tree dcd1cabb85fdf51521ff42374c19007170b095b0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYou = [];
+    for (let i = 0; i < name.length; i++) {
+      thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+  }
+  console.log(writeCards(["Ruth", "Kelvin", "Tangus", "Caudencia"], "surprise"));
+
+  function countDown(firstNumber) {
+    while (firstNumber > 0) {
+      console.log(firstNumber)
+      firstNumber--;
+    }
+    console.log(firstNumber);
+  }
+  countDown(10);
